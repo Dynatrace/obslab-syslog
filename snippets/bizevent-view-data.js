2bf79f69704b3a8ebd713f2f06f81b06d933485a
Adam Gardner <adam.gardner@dynatrace.com>
2025-07-28 09:40:49 +1000

    Deployed 54cc386 with MkDocs version: 1.6.1

--- snippets/bizevent-view-data.js
+++ snippets/bizevent-view-data.js
@@ -1,5 +1,7 @@
 <script>
+var CURRENT_PAGE = "5. view-data";
+
 document.addEventListener('DOMContentLoaded', function() {
-  dynatrace.sendBizEvent('page_load', {"page": "5. view-data"});
+  dynatrace.sendBizEvent('page_load', {"page": CURRENT_PAGE});
 });
 </script>
