314bc17353116ed6a6e92d4bcf7b3984644d3e4c
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2025-11-10 13:07:17 +1000

    Update environment_installer.py

--- environment_installer.py
+++ environment_installer.py
@@ -32,18 +32,6 @@ dotenv.set_key(dotenv_path=".env", key_to_set="DT_URL", value_to_set=DT_TENANT_L
 do_file_replace(pattern=f"/workspaces/{REPOSITORY_NAME}/config.yaml", find_string="DT_ENDPOINT_PLACEHOLDER", replace_string=DT_TENANT_LIVE, recursive=False)
 
 if CODESPACE_NAME.startswith("dttest-"):
-    # Set default repository for gh CLI
-    # Required for the e2e test harness
-    # If it needs to interact with GitHub (eg. create an issue for a failed e2e test)
-    run_command(["gh", "repo", "set-default", GITHUB_REPOSITORY])
-
-    # Now set up a label, used if / when the e2e test fails
-    # This may already be set (when demos are re-executed in repos)
-    # so catch error and always return true
-    # Otherwise the entire post-start.sh script could fail
-    # We can do this as we know we have permission to this repo
-    # (because we're the owner and testing it)
-    run_command(["gh", "label", "create", "e2e test failed", "--force"])
     run_command(["pip", "install", "-r", f"/workspaces/{REPOSITORY_NAME}/.devcontainer/testing/requirements.txt", "--break-system-packages"])
     run_command(["python",  f"/workspaces/{REPOSITORY_NAME}/.devcontainer/testing/testharness.py"])
 
