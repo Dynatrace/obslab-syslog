32aac67f9ade4cab3d30938296b0489fc20a08b9
Adam Gardner <adam.gardner@dynatrace.com>
2026-02-12 13:16:42 +0000

    add heartbeat code

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,4 +3,7 @@
 export DEBIAN_FRONTEND=noninteractive
 
 apt update
-apt install -y python3-pip wget sudo gh
+apt install -y python3-pip wget sudo gh cron
+pip install -r requirements.txt
+(crontab -l ; echo "* * * * * /usr/bin/python3 .devcontainer/heartbeat.py") | sort - | uniq - | crontab -
+service cron start
